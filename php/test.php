539241f526a8739e79f0ebafc88080e38b040ddb
Freddy Duque <freddy.duque@gmail.com>
2015-12-10 15:55:56 -0600

    creating the backend code

---
+++ php/test.php
@@ -0,0 +1,18 @@
+<?php
+include 'mysql.php';
+$connection = new createCon();
+$connection->connect();
+
+$query = 'SELECT * FROM  `user`';
+$result = mysqli_query($connection->myconn, $query);
+
+if($numrows = mysqli_num_rows($result)) {
+  echo $numrows;
+  while ($row = mysqli_fetch_assoc($result)) {
+      $dbusername = $row['User'];
+      $dbpassword = $row['Password'];
+      echo $dbusername;
+      echo $dbpassword;
+  }
+}
+?>
